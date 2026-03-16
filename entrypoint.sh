4edc9ed2d24a7131d96734e906146fbaa696014f
miiyakii <137271151+miiyakii@users.noreply.github.com>
2026-03-16 19:31:41 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # 用环境变量替换 index.html 中的 __API_URL__
-API_URL=${API_URL:-http://localhost:5001}
+API_URL=${API_URL:-}
 # 替换 index.html 占位符
 sed -i "s|__API_URL__|$API_URL|g" /app/index.html
 exec python app.py
